3c8f5362a16f62e3654be012d78024354aaabf1d
Avijit Karmoker <avijitkarmoker050919@gmail.com>
2021-06-20 14:46:48 +0600

    footer section is responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className={footerStyle.footer}>
                 <div className="container">
                     <div className="row m-0">
-                        <div className="col-md-6 col-sm-12">
+                        <div className="col-md-12 col-sm-12 col-lg-6">
                             <div className={footerStyle.image}>
                                 <Image
                                     src="/../public/assets/images/logo.png"
@@ -22,7 +22,7 @@ const Footer = () => {
                             <a href="#" className={footerStyle.link}>{t("home:footer.link")}</a>
                             <p className={footerStyle.contact}>{t("home:footer.contact")}</p>
                         </div>
-                        <div className="col-md-6 col-sm-12">
+                        <div className="col-md-12 col-sm-12 col-lg-6">
                             <div className={footerStyle.footerRight}>
                                 <h5 className={footerStyle.policy}>{t("home:footer.privatePolicy")}</h5>
                                 <p className="m-0">{t("home:footer.copyrightIssue")}</p>
