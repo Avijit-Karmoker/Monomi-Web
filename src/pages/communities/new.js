bb216a7d5d77eb33762484336858526ccb989b14
Simonas Gildutis <simonasgildutis@gmail.com>
2021-07-27 11:36:55 +0300

    add server side translations

--- src/pages/communities/new.js
+++ src/pages/communities/new.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from 'react'
 import Registration from '../../components/AuthModal/components/Registration'
+import { getSession } from 'next-auth/client'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+const namespaces = ['common', 'community']
 
 const New = () => {
   return (
     <div>
       <Registration></Registration>
     </div>
-  );
-};
+  )
+}
 
-export default New;
+export const getServerSideProps = async (context) => {
+  return {
+    props: {
+      session: await getSession(context),
+      ...(await serverSideTranslations(context.locale, namespaces)),
+    },
+  }
+}
+
+export default New
