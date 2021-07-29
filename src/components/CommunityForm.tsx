3a6b9e855d2f8a954e10368932cb8566ed382b4d
Avijit Karmoker <avijitkarmoker050919@gmail.com>
2021-07-29 16:45:25 +0600

    some unhandled error by me

--- src/components/CommunityForm.tsx
+++ src/components/CommunityForm.tsx
@@ -1,20 +1,55 @@
+import { RootState } from '@/store'
+import { EntityAddress, OnboardingUserPayload } from '@/typings'
 import React from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import {
+  useForm,
+  SubmitHandler,
+  Controller,
+  FieldErrors,
+  ChangeHandler,
+} from 'react-hook-form'
+import { FormFeedback, FormGroup, Input, Label } from 'reactstrap'
 import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { defaultLanguage } from '@/config'
+import Select from 'react-select'
+import classnames from 'classnames'
+import { CountryRegionData } from 'react-country-region-selector'
 
 type Inputs = {
   className: string
   children: string
   name: string
-  exampleRequired: string
   profit: string
   entityName: string
   entityCode: string
   entityVat: any
   email: string
+  id: string
 }
 
 export default function CommunityForm() {
+  const { user, locale } = useSelector(
+    ({ authentication: { user }, global: { locale } }: RootState) => ({
+      user,
+      locale,
+    }),
+  )
+  const {
+    control,
+    formState: {},
+  } = useForm<OnboardingUserPayload>({
+    defaultValues: {
+      ...user,
+      address: {
+        country:
+          locale == defaultLanguage.id
+            ? defaultLanguage.code
+            : user?.address?.country,
+      },
+    },
+  })
+
   const {
     register,
     handleSubmit,
@@ -26,7 +61,7 @@ export default function CommunityForm() {
     if (event.target.name === 'email') {
       const isValidEmail =
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-          event.target.value
+          event.target.value,
         )
       console.log(isValidEmail)
     } else {
@@ -41,7 +76,7 @@ export default function CommunityForm() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} style={{height: '833px'}}>
         <label htmlFor='name'>{t('name')}</label> <br />
         <input
           {...register('name', { required: true })}
@@ -94,7 +129,43 @@ export default function CommunityForm() {
         />
         {errors.email && <span>{t('required')}</span>}
         <br />
-        <input type='submit'/>
+        <FormGroup className='form-label-group' style={{marginTop: '1.7em'}}>
+          <Controller
+            control={control}
+            id='address.country'
+            name='address.country'
+            rules={{ required: true }}
+            render={({ onChange, value }) => (
+              <Select
+                defaultValue={
+                  value
+                    ? CountryRegionData.filter(
+                        (item) => value.toLowerCase() === item[1].toLowerCase(),
+                      ).map(([label, value]) => ({ label, value }))
+                    : null
+                }
+                
+                options={CountryRegionData.map(([label, value]) => ({
+                  label,
+                  value,
+                }))}
+                const onChange={(option) => onChange(option?.value)}
+                className={classnames('react-select', {
+                  'is-invalid': !!(errors.address as FieldErrors<EntityAddress>)
+                    ?.country,
+                })}
+                classNamePrefix='select'
+                placeholder={t('select')}
+              />
+            )}
+          />
+          <Input type='hidden' name='address.country' />
+          <Label for='address.country' style={{color: '#A4A1B0 !important'}}>{t('country')}</Label>
+          <FormFeedback>
+            {(errors.address as FieldErrors<EntityAddress>)?.country?.message}
+          </FormFeedback>
+        </FormGroup>
+        <input type='submit' />
       </form>
     </div>
   )
